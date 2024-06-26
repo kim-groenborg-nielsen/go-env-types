763550e1aaa69bd102bd1b0da4ce68298ed1dd86
Kim G. Nielsen <kgn@network-it.dk>
2024-06-26 11:36:24 +0200

    Added go-env-types.go

---
+++ go-env-types.go
@@ -0,0 +1,38 @@
+package go_env_types
+
+import (
+	"os"
+	"strconv"
+)
+
+func GetEnvStr(key, fallback string) string {
+	value, ok := os.LookupEnv(key)
+	if !ok {
+		return fallback
+	}
+	return value
+}
+
+func GetEnvInt(key string, fallback int) int {
+	value, ok := os.LookupEnv(key)
+	if !ok {
+		return fallback
+	}
+	i, err := strconv.Atoi(value)
+	if err != nil {
+		return fallback
+	}
+	return i
+}
+
+func GetEnvBool(key string, fallback bool) bool {
+	value, ok := os.LookupEnv(key)
+	if !ok {
+		return fallback
+	}
+	b, err := strconv.ParseBool(value)
+	if err != nil {
+		return fallback
+	}
+	return b
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kim-groenborg-nielsen/go-env-types
+
+go 1.22.1
